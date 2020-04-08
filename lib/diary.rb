89c46f6a7c151084528d43babfff24d9ecbfdd52
Faye Carter <fayecarter333@gmail.com>
2020-04-08 17:08:31 +0100

    Refactor add method to use database

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,20 +1,29 @@
 require_relative 'entry'
+require 'pg'
 
 class Diary
 
-  attr_reader :entries
-
-  def initialize
-    @entries = []
+  def self.all
+    connection = PG.connect(dbname: 'diary')
+    result = connection.exec('SELECT * FROM Entries')
+    result.map{ |entry| entry['title'] }
   end
 
-  def add(title)
-    entry = Entry.new(title)
-    @entries << entry.title
-  end
 
-  def list_titles
-    @entries
-  end
+
+  # attr_reader :entries
+
+  # def initialize
+  #   @entries = []
+  # end
+
+  # def add(title)
+  #   entry = Entry.new(title)
+  #   @entries << entry.title
+  # end
+
+  # def list_titles
+  #   @entries
+  # end
 
 end
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -2,15 +2,19 @@ require 'diary'
 
 describe Diary do
   
-  it 'can add a dairy entry' do
-    subject.add("New Entry")
-    expect(subject.entries).to include "New Entry"
-  end
+  # it 'can add a dairy entry' do
+  #   subject.add("New Entry")
+  #   expect(subject.entries).to include "New Entry"
+  # end
 
-  it 'can list dairy entry titles' do
-    subject.add("First Entry")
-    subject.add("Second Entry")
-    expect(subject.list_titles).to eq ["First Entry","Second Entry"]
+  # it 'can list dairy entry titles' do
+  #   subject.add("First Entry")
+  #   subject.add("Second Entry")
+  #   expect(subject.list_titles).to eq ["First Entry","Second Entry"]
+  # end
+
+  it 'can add a dairy entry' do
+    expect(Diary.all).to eq ["First Entry", "Second Entry"]
   end
 
 end
