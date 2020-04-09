c9b65ebbf1e6595122e54e7d7996390d0d00a4a8
Faye Carter <fayecarter333@gmail.com>
2020-04-09 10:36:26 +0100

    entries page setup

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
   get '/' do
     erb(:index)
   end
+
+  post '/get' do
+    erb :index
+    redirect '/entries'
+  end
+
+  get '/entries' do
+    erb(:entries)
+  end
 end
