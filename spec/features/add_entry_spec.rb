489adc07e31ac8bb656afd2d9663ad56de96fc17
Faye Carter <fayecarter333@gmail.com>
2020-04-09 11:16:31 +0100

    test - new entry page to have a body field - fail

--- spec/features/add_entry_spec.rb
+++ spec/features/add_entry_spec.rb
@@ -1,6 +1,13 @@
 feature 'entries page loads' do
-  scenario 'with entry' do
+  
+  scenario 'with entry title field' do
     visit('/new')
     expect(page).to have_field :title
   end
+
+  scenario 'with entry body field' do
+    visit('/new')
+    expect(page).to have_field :body
+  end
+
 end
